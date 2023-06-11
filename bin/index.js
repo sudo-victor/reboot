879fadc4ccda7fe652ac09217b7a52ef517ac5da
Victor de Souto <victor.souto@hotmail.com>
2023-06-11 12:57:50 -0300

    cleanup

--- bin/index.js
+++
@@ -1,9 +0,0 @@
-const { Watch } = require("./lib/watch")
-const path = require("path")
-
-const fileByCLI = process.argv[2]
-const file = path.join(__dirname, fileByCLI)
-
-const watchman = new Watch()
-
-watchman.compile(process.cwd(), file)
--- server.js
+++
@@ -1,7 +0,0 @@
-const http = require("node:http")
-
-const app = http.createServer((req, res) => {
-  res.end("Hello, world 3!")
-})
-
-app.listen(8080, "localhost", () => console.log("Server listening on 8080"))
